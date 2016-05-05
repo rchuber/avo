b2ef058d05a7bc455b77429447835ee1d696ae70
Ryan Huber <rchuber@gmail.com>
2016-05-05 12:32:40 +0100

    Create simple swipe paging interface

--- avo/avo/ViewController.swift
+++ avo/avo/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Add Senior Dashboard as child view controller to the paged scroll view
+        let SeniorDashboardViewController = UIViewController(nibName: "SeniorDashboardViewController", bundle: nil)
+        self.addChildViewController(SeniorDashboardViewController)
+        self.scrollView.addSubview(SeniorDashboardViewController.view)
+        SeniorDashboardViewController.didMoveToParentViewController(self)
+        
+        // Add a second Senior Dashboard to the scroll view
+        let SeniorDashboardViewController1 = UIViewController(nibName: "SeniorDashboardViewController", bundle: nil)
+        var frame1 = SeniorDashboardViewController1.view.frame
+        // push this page over the width of the first screen
+        frame1.origin.x = self.view.frame.size.width
+        SeniorDashboardViewController1.view.frame = frame1
+        SeniorDashboardViewController1.view.backgroundColor = UIColor.yellowColor() // for testing
+        self.addChildViewController(SeniorDashboardViewController1)
+        self.scrollView.addSubview(SeniorDashboardViewController1.view)
+        SeniorDashboardViewController1.didMoveToParentViewController(self)
+     
+        // Add a second Senior Dashboard to the scroll view
+        let SeniorDashboardViewController2 = UIViewController(nibName: "SeniorDashboardViewController", bundle: nil)
+        var frame2 = SeniorDashboardViewController2.view.frame
+        // push this page over the width of the first screen
+        frame2.origin.x = self.view.frame.size.width * 2
+        SeniorDashboardViewController2.view.frame = frame2
+        SeniorDashboardViewController2.view.backgroundColor = UIColor.greenColor() // for testing
+        self.addChildViewController(SeniorDashboardViewController2)
+        self.scrollView.addSubview(SeniorDashboardViewController2.view)
+        SeniorDashboardViewController1.didMoveToParentViewController(self)
+        
+        // set width to be the width of all view controllers side by side
+        self.scrollView.contentSize = CGSize(width: self.view.frame.size.width * 3, height: self.view.frame.size.height)
+
     }
 
     override func didReceiveMemoryWarning() {
