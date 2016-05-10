d4f2c39b7c1aa612c9cc8d3b6fb42e8645297507
Ryan Huber <rchuber@gmail.com>
2016-05-10 22:41:40 +0100

    Added actual buttons, changed scrollview to pagecontroller

--- avo/avo/SeniorDashboardViewController.swift
+++ avo/avo/SeniorDashboardViewController.swift
@@ -6,16 +6,22 @@ import UIKit
 
 class SeniorDashboardViewController: UIViewController {
     
-    var testThing: String = "test!"
+    public var seniorIndex: Int!
+    public var seniorName: String!
     
     @IBOutlet weak var seniorCollectionView: UICollectionView!
+    @IBOutlet weak var seniorNameLabel: UILabel!
     
-    //private var numbers: [Int] = []
+    private var cellIdentifiers: [String] = ["ActivityCell", "MedicationCell","VitalsCell","MentalCell","SleepCell","MessagesCell"]
     
     private var longPressGesture: UILongPressGestureRecognizer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        seniorCollectionView.dataSource = self
+    
+        seniorNameLabel.text = seniorName
         
         /*for i in 0...100 {
             numbers.append(i)
@@ -44,11 +50,59 @@ class SeniorDashboardViewController: UIViewController {
     }
     
 }
-/*
- extension SeniorDashboardViewController: UICollectionViewDataSource {
+
+ /*extension SeniorDashboardViewController: UICollectionViewDataSource {
  
- func seniorCollectionView(seniorCollectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
- return numbers.count
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+    {
+        
+        //let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ActivityCell", forIndexPath: indexPath)// as! ActivityCell
+        
+        
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(identifier, forIndexPath: indexPath) as! UICollectionViewCell
+        cell.backgroundColor = UIColor.redColor()
+        
+        return cell
+        
+        
+        
+        /*print("here!")
+        
+        var bcolor : UIColor = UIColor( red: 0.2, green: 0.2, blue:0.2, alpha: 0.3 )
+        
+        cell.layer.borderColor = bcolor.CGColor
+        cell.layer.borderWidth = 0.5
+        cell.layer.cornerRadius = 3
+        
+        cell.backgroundColor=UIColor.whiteColor()
+        
+        
+        //cell.text.text = Data[indexPath.row].code
+        //cell.country.text = Data[indexPath.row].country
+        
+        return cell
+        */
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return 2
+    }
+    
+    //func collectionView(collectionView: UICollectionView,
+                       // layout collectionViewLayout: UICollectionViewLayout,
+                       //        sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        //return CGSize(width: 100  , height: 50)
+        
+    //}
+    
+    /*
+    
+    func seniorCollectionView(seniorCollectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 6
  }
  
  func seniorCollectionView(seniorCollectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
@@ -63,9 +117,28 @@ class SeniorDashboardViewController: UIViewController {
  
  func seniorCollectionView(seniorCollectionView: UICollectionView, moveItemAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
  
- let temp = numbers.removeAtIndex(sourceIndexPath.item)
- numbers.insert(temp, atIndex: destinationIndexPath.item)
- }
+    let temp = numbers.removeAtIndex(sourceIndexPath.item)
+    numbers.insert(temp, atIndex: destinationIndexPath.item)
+ }*/
  
  }
  */
+
+// MARK:- UICollectionViewDataSource Delegate
+extension SeniorDashboardViewController: UICollectionViewDataSource {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cellIdentifiers.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        var identifier = cellIdentifiers[indexPath.item]
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(identifier, forIndexPath: indexPath) as! UICollectionViewCell
+        //cell.backgroundColor = UIColor.redColor()
+    
+    return cell
+    }
+}
+
+
+ 
--- avo/avo/ViewController.swift
+++ avo/avo/ViewController.swift
@@ -6,24 +6,110 @@
 //  Copyright © 2016 MHCI. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
+class ManagePageViewController: UIPageViewController {
+    var seniors = ["Granny", "Grammy", "Grampy"]
+    var currentIndex: Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self
+        self.currentIndex = 0
+        
+        // 1
+        if let viewController = viewSeniorDashboardViewController(currentIndex ?? 0) {
+            let viewControllers = [viewController]
+            // 2
+            setViewControllers(
+                viewControllers,
+                direction: .Forward,
+                animated: false,
+                completion: nil
+            )
+        }
+    }
+    
+    func viewSeniorDashboardViewController(index: Int) -> SeniorDashboardViewController? {
+        if let storyboard = storyboard,
+            page = storyboard.instantiateViewControllerWithIdentifier("seniorDashboardViewStoryboard")
+                as? SeniorDashboardViewController {
+            
+            page.seniorName = seniors[index]
+            page.seniorIndex = index
+            return page
+        }
+        return nil
+    }
+    
+    // MARK: UIPageControl
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return seniors.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return currentIndex ?? 0
+    }
+    
+}
 
+//MARK: implementation of UIPageViewControllerDataSource
+extension ManagePageViewController: UIPageViewControllerDataSource {
+    // 1
+    func pageViewController(pageViewController: UIPageViewController,
+                            viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        if let viewController = viewController as? SeniorDashboardViewController {
+            var index = viewController.seniorIndex
+            guard index != NSNotFound && index != 0 else { return nil }
+            index = index - 1
+            return viewSeniorDashboardViewController(index)
+        }
+        return nil
+    }
+    
+    // 2
+    func pageViewController(pageViewController: UIPageViewController,
+                            viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        if let viewController = viewController as? SeniorDashboardViewController {
+            var index = viewController.seniorIndex
+            guard index != NSNotFound else { return nil }
+            index = index + 1
+            guard index != seniors.count else {return nil}
+            return viewSeniorDashboardViewController(index)
+        }
+        return nil
+    }
+}
+
+
+
+/*
+
+class ViewController: UIPageViewController {
+
+    
     @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Add Senior Dashboard as child view controller to the paged scroll view
-        let SeniorDashboardViewController0 = SeniorDashboardViewController(nibName: "SeniorDashboardViewController", bundle: nil)
+        //let SeniorDashboardViewController0 = SeniorDashboardViewController()
+        
+        let SeniorDashboardViewController0:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("seniorDashboardViewStoryboard") as UIViewController
+        //self.presentViewController(SeniorDashboardViewController0, animated: false, completion: nil)
+        // .instantiatViewControllerWithIdentifier() returns AnyObject! this must be downcast to utilize it
         self.addChildViewController(SeniorDashboardViewController0)
         self.scrollView.addSubview(SeniorDashboardViewController0.view)
         SeniorDashboardViewController0.didMoveToParentViewController(self)
-        
+
         // Add a second Senior Dashboard to the scroll view
-        
-        let SeniorDashboardViewController1 = SeniorDashboardViewController(nibName: "SeniorDashboardViewController", bundle: nil)
+        let SeniorDashboardViewController1:SeniorDashboardViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("seniorDashboardViewStoryboard") as! SeniorDashboardViewController
+
         var frame1 = SeniorDashboardViewController1.view.frame
         // push this page over the width of the first screen
         frame1.origin.x = self.view.frame.size.width
@@ -37,7 +123,8 @@ class ViewController: UIViewController {
      
         
         // Add a third Senior Dashboard to the scroll view
-        let SeniorDashboardViewController2 = SeniorDashboardViewController(nibName: "SeniorDashboardViewController", bundle: nil)
+        let SeniorDashboardViewController2:SeniorDashboardViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("seniorDashboardViewStoryboard") as! SeniorDashboardViewController
+
         var frame2 = SeniorDashboardViewController2.view.frame
         // push this page over the width of the first screen
         frame2.origin.x = self.view.frame.size.width * 2
@@ -58,7 +145,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+ }
+*/
 
-
-}
 
